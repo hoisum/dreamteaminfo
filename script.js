8c928d56575781a4f4b6869afe74e298c93acb75
hoisum <wupaulinec@gmail.com>
2015-07-20 18:18:00 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+var dreamteam = {};
+
+dreamteam.rc = {
+	firstName: "R.C",
+    age: "33",
+    title: "Software Engineer",
+    city: "Redmond",
+    birthday: new Date(1982, 5, 11);
+    faveFoods: ['burgers', 'pasta', 'soylent']
+};
+
+dreamteam.pauline = {
+	firstName: "Pauline",
+    age: "26",
+    title: "Software Engineer... or something",
+    city: "Seattle",
+    birthday: new Date(1988, 10, 10);
+    faveFoods: ['noodles,' 'more booze']
+};
+
+coworkers.tae = {
+	firstName: "Tae",
+    age: "41",
+    title: "THE BOSSMAN!",
+    city: "Renton",
+    birthday: null,
+    faveFoods: ['kimchi', 'kungpao chicken', 'booze']
+};
+
+
+
